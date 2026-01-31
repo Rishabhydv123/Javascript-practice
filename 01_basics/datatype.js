c8eaa0a3e6898b1635a0327bd2caf00e872d6525
Rishabhydv123 <rishabhyadav55888@gmail.com>
2026-01-31 12:55:23 +0530

    update

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -2,14 +2,12 @@
 
 // alert( 3 + 3) // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
+console.log(3 + 3) // code readability should be high
 
-console.log("Hitesh")
+// console.log("Rishabh")
 
 
-let name = "hitesh"
+let name = "Rishabh"
 let age = 18
 let isLoggedIn = false
 let state;
@@ -25,5 +23,29 @@ let state;
 
 // object
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+const student = {
+    fullname: "Rishabbh Yadav",
+    age:22,
+    cgpa:8.5,
+    ispass:true
+};
+
+student ["age"]= student ["age"]-1
+
+console.log(student.age);
+
+let a= 5,
+b= 10
+console.log(b);
+
+const product = {
+    title: "Pen",
+    rating:5.5,
+    offer:5,
+    price:55
+}
+
+console.log(product);
---
+++ 01_basics/operator.js
@@ -0,0 +1,53 @@
+//operator
+//(used to perform some operation on data)
+
+            // Arithmetic operator
+                   //  (+,-,*,/)
+
+let a = 5;
+let b = 2;
+
+// console.log("a = ", a, "& b = ", b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+
+// modulud (%)
+console.log("a % b =", a % b);
+
+// unary operator
+console.log("a = ", a, "& b = ", b);
+// a = a + 1;//6
+console.log("a =", a);//6
+// a++; //+
+console.log("a =", a);
+// a-1;
+console.log("a =", a);
+// a--;
+console.log("a =", a);
+++a;
+console.log("++a =", ++a);
+// --a;
+console.log("--a =", --a);
+
+
+//Assignment operators
+
+let a1 = 5;
+let b1 = 2;
+
+a1 += 6; //a = a+6
+console.log("a1 =", a1); //11
+
+a1 -= 6; //a = a-6
+console.log("a1 =", a1); //5
+
+a1 *= 6; //a = a*6
+console.log("a1 =", a1); //30
+
+a1 %= 6; //a = a%6
+console.log("a1 =", a1); //0
+
+a1 ** 4; //a = a**4
+console.log("a1 =", a1); //0
